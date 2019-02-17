1a75b00773e12e46bc856436b7cf902c1db8655a
Nico Guba <nguba@mac.com>
2019-02-17 12:38:24 +0000

    first import

---
+++ presentation/src/test/java/me/nguba/plant/PlantApplicationTests.java
@@ -0,0 +1,16 @@
+package me.nguba.plant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
