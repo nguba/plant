494ca1b4908ccd1a9ceb117d5af2446735ff8488
Nico Guba <nguba@mac.com>
2019-02-17 13:25:04 +0000

    configured for junit5

--- presentation/src/test/java/me/nguba/plant/PlantApplicationTests.java
+++ presentation/src/test/java/me/nguba/plant/PlantApplicationTests.java
@@ -1,16 +1,19 @@
 package me.nguba.plant;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class PlantApplicationTests {
+public class PlantApplicationTests
+{
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads()
+    {
+    }
 
 }
